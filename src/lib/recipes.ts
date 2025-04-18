f3fa09753aa9b3902b25231aabb7839946531bd8
Jamie Davies <jamie@jagd.me>
2025-04-18 21:38:21 +0100

    Start using cooklang-chef CLI to parse recipe files as JSON

--- src/lib/recipes.ts
+++ src/lib/recipes.ts
@@ -1,36 +1,49 @@
 import { promises as fs } from "fs";
-import path from "path";
+import { promisify } from "util";
+import { exec } from "child_process";
+
+const pexec = promisify(exec);
 
 type Recipe = {
   content: string;
   slug: string;
 };
 
-const RECIPES_PATH = "recipes";
+const RECIPES_BASE_PATH = "recipes";
+
+async function getRecipePaths(): Promise<string[]> {
+  const filenames = await fs.readdir(RECIPES_BASE_PATH);
+  return filenames.map((filename) => `${RECIPES_BASE_PATH}/${filename}`);
+}
+
+function calculateSlug(path: string): string {
+  return path
+    .replace(new RegExp(`^${RECIPES_BASE_PATH}/`), "")
+    .replace(/\.cook$/, "");
+}
+
+export async function getRecipe(
+  recipePath: string,
+): Promise<Recipe | undefined> {
+  const jsonOutput = await pexec(`chef recipe ${recipePath} --format json`);
+  return {
+    content: jsonOutput.stdout.trim(),
+    slug: calculateSlug(recipePath),
+  };
+}
 
 export async function getRecipes(): Promise<Recipe[]> {
-  const filenames = await fs.readdir(RECIPES_PATH);
+  const recipePaths = await getRecipePaths();
   const recipes: Recipe[] = [];
 
   await Promise.all(
-    filenames.map(async (filename) => {
-      const recipePath = path.join(RECIPES_PATH, filename);
-      const recipe = await fs.readFile(recipePath);
-      const slug = recipePath
-        .replace(new RegExp(`^${RECIPES_PATH}/`), "")
-        .replace(/\.cook$/, "");
-
-      recipes.push({
-        content: recipe.toString(),
-        slug,
-      });
+    recipePaths.map(async (recipePath) => {
+      const recipe = await getRecipe(recipePath);
+      if (recipe) {
+        recipes.push(recipe);
+      }
     }),
   );
 
   return recipes;
-}
-
-export async function getRecipe(slug: string): Promise<Recipe | undefined> {
-  const recipes = await getRecipes();
-  return recipes.find((recipe) => recipe.slug === slug);
 }
